6cd20b6b45bb0c1549f54e4a9c04dfc3ca2b1a35
vidakpop <daviskpop@gmail.com>
2025-03-18 11:24:04 +0300

    add ImageGallery model with title, description, and image fields

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class ImageGallery(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='gallery/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
